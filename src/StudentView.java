a8354177c635b0a4fe9e73897dc96bf3c3b3a81a
Fernando Correa <correahuinchofernandoluis@gmail.com>
2026-08-25 21:51:45 -0500

    feat: Actualizar la clase StudentView

---
+++ src/StudentView.java
@@ -0,0 +1,7 @@
+public class StudentView {
+
+    public void mostrarEstudiante(String nombre, String carrera) {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Carrera: " + carrera);
+    }
+}
